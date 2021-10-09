9b5289831eb7990b7f74a8003b89f8fd819af7a7
Tomer Sedan <tomer.sedan@protonmail.com>
2021-10-09 00:54:32 -0400

    z-index and basic camera

--- main.py
+++ main.py
@@ -7,16 +7,12 @@ game = PP.Game()
 scene = Scene()
 game.scene_manager.add_scene(scene)
 
-sprite = Sprite(100, 100, "./Tinmarr.jpg")
+sprite = Sprite("./Tinmarr.jpg" ,100, 100, 0)
 
 def custom_update():
-    sprite.state["show"] = Input.is_pressed("SPACE")
-
-def custom_draw():
-    if sprite.state.get("show"): GD.update(sprite.image, sprite.position)
+    sprite.z_index = 1 - Input.is_pressed("SPACE")
 
 sprite.update = custom_update
-sprite.draw = custom_draw
 scene.sprites.append(sprite)
 
 def test_handler():
---
+++ pgp/scenes/camera.py
@@ -0,0 +1,5 @@
+class Camera:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.z = 0
