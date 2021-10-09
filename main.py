2152f1320aa464f626a0e8802b598bf5e33ab47b
Tomer Sedan <tomer.sedan@protonmail.com>
2021-10-08 23:48:14 -0400

    remove redundant newlines

--- main.py
+++ main.py
@@ -9,15 +9,12 @@ game.scene_manager.add_scene(scene)
 
 sprite = Sprite(100, 100, "./Tinmarr.jpg")
 
-
 def custom_update():
     if Input.is_pressed("SPACE"):
         game.broadcast.broadcast_event("test")
 
-
 sprite.update = custom_update
 scene.sprites.append(sprite)
-
 
 def test_handler():
     print("Yeet")
