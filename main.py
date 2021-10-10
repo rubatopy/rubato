4342e7e9b1b9d042c6917bb810bc798030f4eb59
Tomer Sedan <tomer.sedan@protonmail.com>
2021-10-10 01:21:52 -0400

    update main

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def custom_update():
         scene.camera.zoom = 2
     elif Input.is_pressed("s"):
         scene.camera.zoom = 0.5
+    else:
+        scene.camera.zoom = 1
 
 
 sprite.update = custom_update
