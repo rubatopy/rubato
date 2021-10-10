5a2470189d2528d8986261e9a4878aa9b041d588
Tomer Sedan <tomer.sedan@protonmail.com>
2021-10-10 00:04:28 -0400

    index sprites by id

--- pgp/scenes/scene.py
+++ pgp/scenes/scene.py
@@ -7,30 +7,40 @@ class Scene:
     """
 
     def __init__(self):
-        self.sprites = []
+        self.sprites = {}
+        self.min_id = 0
         self.camera = Camera()
 
     # TODO Sprite remove function. Also, index sprites by ID like in scenemanager
-    def add(self, sprite: Sprite):
+    def add(self, sprite: Sprite, sprite_id: int or str = None):
         """
         Adds a sprite to the scene.
 
+        :param sprite_id: The id of the sprite
         :param sprite: The sprite object to be added.
         """
-        self.sprites.append(sprite)
+        if sprite_id is None:
+            sprite_id = self.min_id
+            self.min_id += 1
+
+        if sprite_id in self.sprites.keys():
+            raise ValueError(f"The sprite id {sprite_id} is not unique in this scene")
+
+        self.sprites[sprite_id] = sprite
+        return sprite_id
 
     def update(self):
         """
         The update loop for this scene.
         """
-        for sprite in self.sprites:
+        for sprite in self.sprites.values():
             sprite.update()
 
     def draw(self):
         """
         The draw loop for this scene.
         """
-        for sprite in sorted(self.sprites, key=lambda spr: spr.pos.z):
+        for sprite in sorted(self.sprites.values(), key=lambda spr: spr.pos.z):
             if sprite.pos.z > self.camera.pos.z:
                 break
             sprite.draw(self.camera)
