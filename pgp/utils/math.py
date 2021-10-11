c2e9ae1d98fac0196e95891ed30016ac8350e39f
Tomer Sedan <tomer.sedan@protonmail.com>
2021-10-11 10:53:10 -0400

    refactor math

--- pgp/utils/math.py
+++ pgp/utils/math.py
@@ -14,19 +14,20 @@ class PgpMath:
     @staticmethod
     def lerp(lower, upper, t):
         """
-        interpolates between lower and upper by t
-        :param lower: the lower bound
-        :param upper: the upper bound
-        :param t: how far you go between lower and upper
+        Linearly interpolates between lower and upper bounds by t
+
+        :param lower: The lower bound
+        :param upper: The upper bound
+        :param t: How far you go between lower and upper
         """
         return (t * upper) + ((1 - t) * lower)
 
     @staticmethod
-    def deg_2_rad(deg):
+    def deg_to_rad(deg):
         return deg * math.pi / 180
 
     @staticmethod
-    def rad_2_deg(rad):
+    def rad_to_deg(rad):
         return rad * 180 / math.pi
 
 
@@ -39,7 +40,6 @@ class Vector(Vector2):
     :param z: The z coordinate.
     """
 
-    # TODO ask tomer if its ok to have float precision in the Vectors was int
     def __init__(self, x: float = 0, y: float = 0, z: float = 0):
         self.x, self.y, self.z = x, y, z
 
@@ -69,7 +69,7 @@ class Vector(Vector2):
         return self.x, self.y
 
     def __str__(self):
-        return f"({self.x}, {self.y})"
+        return f"({self.x}, {self.y}, {self.z})"
 
     def __mul__(self, other):
         if isinstance(other, int) or isinstance(other, float):
@@ -78,42 +78,35 @@ class Vector(Vector2):
             return Vector(self.x * other.x, self.y * other.y, self.z)
 
     @staticmethod
-    @property
     def ZERO():
         return Vector(0, 0)
 
     @staticmethod
-    @property
     def ONE():
         return Vector(1, 1)
 
     @staticmethod
-    @property
     def TWO():
         return Vector(2, 2)
 
     @staticmethod
-    @property
     def UP():
         return Vector(0, -1)
 
     @staticmethod
-    @property
     def LEFT():
         return Vector(-1, 0)
 
     @staticmethod
-    @property
     def DOWN():
         return Vector(0, 1)
 
     @staticmethod
-    @property
     def RIGHT():
         return Vector(1, 0)
 
-    def __equals(self, v):
-        return self.y == v.y and self.x == v.y
+    def __equals2(self, v):
+        return self.y == v.y and self.x == v.x
 
     def clamp(self, lower, upper):
         """
@@ -124,22 +117,22 @@ class Vector(Vector2):
         :return: None
         """
         if not isinstance(lower, Vector):
-            Vector(*lower)
+            lower = Vector(*lower)
         if not isinstance(upper, Vector):
-            Vector(*upper)
+            upper = Vector(*upper)
         self.x = PgpMath.clamp(self.x, lower.x, upper.x)
         self.y = PgpMath.clamp(self.y, lower.y, upper.y)
 
     def __eq__(self, other):
-        return False if (other is None or not isinstance(other, Vector)) else self.__equals(other)
+        return False if (other is None or not isinstance(other, Vector)) else self.__equals2(other)
 
     @staticmethod
-    def from_direction_distance(direction: float, distance: float):
+    def from_radial(angle: float, magnitude: float):
         """
         Gives you a Vector from the given direction and distance
-        :param direction: direction of vector in radians
-        :param distance: length of vector
+
+        :param angle: Direction of vector in radians
+        :param magnitude: Length of vector
         :return: Vector from the given direction and distance
         """
-        x, y = math.cos(direction) * distance, math.sin(direction) * distance
-        return Vector(x, y)
+        return Vector(math.cos(angle) * magnitude, math.sin(angle) * magnitude)
