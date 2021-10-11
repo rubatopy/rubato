16b556110239256d845cf2aed9e37e9a68a780df
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2021-10-11 16:04:27 -0400

    Process

--- pgp/utils/processing.py
+++ pgp/utils/processing.py
@@ -1,6 +1,6 @@
 class Process:
     @staticmethod
-    def check_types(annotations, args):
+    def check_types(func, args):
         for arg in args.keys():
-            if not isinstance(args[arg], annotations[arg]):
-                raise TypeError(f"{args[arg]} is not of type {annotations[arg].__name__}")
+            if not isinstance(args[arg], func.__annotations__[arg]):
+                raise TypeError(f"{args[arg]} is not of type {func.__annotations__[arg].__name__}")
