cdb2b5d8577afcab8bc633cf0654c16633dc9c0d
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2021-10-11 16:15:46 -0400

    Process Again Again

--- pgp/utils/processing.py
+++ pgp/utils/processing.py
@@ -3,5 +3,8 @@ class Process:
     def check_types(func, args):
         for arg in args.keys():
             if arg != "self" and arg[:2] != "__":
-                if not isinstance(args[arg], func.__annotations__[arg]):
-                    raise TypeError(f"{args[arg]} is not of type {func.__annotations__[arg].__name__}")
+                try:
+                    if not isinstance(args[arg], func.__annotations__[arg]):
+                        raise TypeError(f"{args[arg]} is not of type {func.__annotations__[arg].__name__}")
+                except KeyError:
+                    raise SyntaxError(f"The argument {arg} does not have a type annotation. (Use any to bypass this)")
