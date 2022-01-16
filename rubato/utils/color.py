dfe4468cccafecdb29b72f5fc22ccbf496b19ebf
mrElnekave <ye41550@gmail.com>
2022-01-16 15:45:09 -0800

    removed State reference

--- rubato/utils/color.py
+++ rubato/utils/color.py
@@ -1,6 +1,66 @@
 from rubato.utils import PMath
 
 
+class HSV:
+    def __init__(self, h, s, v):
+        self.h: float = h
+        self.s: float = s
+        self.v: float = v
+
+    def set(self, value):
+        self.h = value
+        self.s = value
+        self.v = value
+
+    def __eq__(self, other):
+        if type(other) == type(self):
+            return \
+                abs(self.h - other.h) < 0.0001 and \
+                abs(self.s - other.s) < 0.0001 and \
+                abs(self.v - other.v) < 0.0001
+        return False
+
+    @property
+    def values(self):
+        return [self.h, self.s, self.v]
+
+    def check_values(self):
+        self.h = PMath.clamp(self.h, 0, 255)
+        self.s = PMath.clamp(self.s, 0, 255)
+        self.v = PMath.clamp(self.v, 0, 255)
+
+
+class RGB:
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.check_values()
+
+    def set(self, value):
+        self.r = value
+        self.g = value
+        self.b = value
+        self.check_values()
+
+    def __eq__(self, other):
+        if type(other) == type(self):
+            return \
+                abs(self.r - other.r) < 0.0001 and \
+                abs(self.g - other.g) < 0.0001 and \
+                abs(self.b - other.b) < 0.0001
+        return False
+
+    @property
+    def values(self):
+        return [self.r, self.g, self.b]
+
+    def check_values(self):
+        self.r = PMath.clamp(self.r, 0, 255)
+        self.g = PMath.clamp(self.b, 0, 255)
+        self.b = PMath.clamp(self.g, 0, 255)
+
+
 class Color:
     # colors from https://www.rapidtables.com/web/color/RGB_Color.html
     black = (0, 0, 0)
@@ -20,63 +80,23 @@ class Color:
     teal = (0, 128, 128)
     navy = (0, 0, 128)
 
-    class HSV:
+    # @staticmethod
+    # def lerp(a: Color.RGB, b: Color.RGB, t):
+    #     t = PMath.clamp(t, 0, 1)
+    #     return RGB(
+    #     a.r + (b.r - a.r) * t,
+    #     a.g + (b.g - a.g) * t,
+    #     a.b + (b.b - a.b) * t,
+    #     a.a + (b.a - a.a) * t
+    #     )
+
+    class HSV(HSV):
         def __init__(self, h, s, v):
-            self.h: float = h
-            self.s: float = s
-            self.v: float = v
+            super().__init__(h,s,v)
 
-        def set(self, value):
-            self.h = value
-            self.s = value
-            self.v = value
-
-        def __eq__(self, other):
-            if type(other) == type(self):
-                return \
-                    abs(self.h - other.h) < 0.0001 and \
-                    abs(self.s - other.s) < 0.0001 and \
-                    abs(self.v - other.v) < 0.0001
-            return False
-
-        @property
-        def values(self):
-            return [self.h, self.s, self.v]
-
-        def check_values(self):
-            self.h = PMath.clamp(self.h, 0, 255)
-            self.s = PMath.clamp(self.s, 0, 255)
-            self.v = PMath.clamp(self.v, 0, 255)
-
-    class RGB:
+    class RGB(RGB):
         def __init__(self, r, g, b):
-            self.r = r
-            self.g = g
-            self.b = b
-            self.check_values()
-
-        def set(self, value):
-            self.r = value
-            self.g = value
-            self.b = value
-            self.check_values()
-
-        def __eq__(self, other):
-            if type(other) == type(self):
-                return \
-                    abs(self.r - other.r) < 0.0001 and \
-                    abs(self.g - other.g) < 0.0001 and \
-                    abs(self.b - other.b) < 0.0001
-            return False
-
-        @property
-        def values(self):
-            return [self.r, self.g, self.b]
-
-        def check_values(self):
-            self.r = PMath.clamp(self.r, 0, 255)
-            self.g = PMath.clamp(self.b, 0, 255)
-            self.b = PMath.clamp(self.g, 0, 255)
+            super().__init__(r, g, b)
 
     @staticmethod
     def rgb_to_hsv(color_in: RGB):
