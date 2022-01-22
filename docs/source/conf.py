ad3da1c112323273f14d1ccf6af0b7601e41da19
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2022-01-22 00:30:31 -0800

    Add Robots

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,6 +54,7 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_extra_path = ["../robots.txt"]
 
 add_module_names = False
 
