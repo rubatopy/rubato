23beb5294d41c487f7789d7053de3c1a94eaf730
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2022-03-09 21:17:55 -0800

    Update physics_demo.py

--- physics_demo.py
+++ physics_demo.py
@@ -1,7 +1,7 @@
 """Physics Demo... basically a ball pit"""
 from random import randint
 import rubato as rb
-from rubato import Game, Vector
+from rubato import Game, Vector, Color
 
 num_balls = 50
 rb.init({
@@ -19,45 +19,50 @@ rb.Game.scenes.set("main")
 top = rb.Sprite({
     "pos": Vector(300, -25)
 }).add_component(
-    rb.Rectangle({
-        "dims": Vector(600, 100),
-        "color": rb.Color.maroon
-    })).add_component(rb.Polygon.generate_rect(600, 100))
+    rb.Polygon({
+        "verts": rb.Polygon.generate_rect(600, 100),
+        "debug": False,
+        "color": Color.maroon,
+    }))
 
 bottom = rb.Sprite({
     "pos": Vector(300, 625)
 }).add_component(
-    rb.Rectangle({
-        "dims": Vector(600, 100),
-        "color": rb.Color.maroon
-    })).add_component(rb.Polygon.generate_rect(600, 100))
+    rb.Polygon({
+        "verts": rb.Polygon.generate_rect(600, 100),
+        "debug": False,
+        "color": Color.maroon,
+    }))
 
 left = rb.Sprite({
     "pos": Vector(-25, 300)
 }).add_component(
-    rb.Rectangle({
-        "dims": Vector(100, 600),
-        "color": rb.Color.maroon
-    })).add_component(rb.Polygon.generate_rect(100, 600))
+    rb.Polygon({
+        "verts": rb.Polygon.generate_rect(100, 600),
+        "debug": False,
+        "color": Color.maroon,
+    }))
 
 right = rb.Sprite({
     "pos": Vector(625, 300)
 }).add_component(
-    rb.Rectangle({
-        "dims": Vector(100, 600),
-        "color": rb.Color.maroon
-    })).add_component(rb.Polygon.generate_rect(100, 600))
+    rb.Polygon({
+        "verts": rb.Polygon.generate_rect(100, 600),
+        "debug": False,
+        "color": Color.maroon,
+    }))
 
 balls = []
 for i in range(num_balls):
     ball = rb.Sprite({
-        "pos": Vector(randint(100, 500), randint(100, 500))
-    }).add_component(rb.Circle(25)).add_component(
-        rb.RigidBody({
-            "bouncyness": 1,
-            "gravity": Vector()
-        }))
-    ball.get_component(rb.Hitbox).debug = True
+        "pos": Vector(randint(50, 550), randint(50, 550))
+    }).add_component(rb.Circle({
+        "radius": 25,
+        "color": Color.random
+    })).add_component(rb.RigidBody({
+        "bounciness": 1,
+        "gravity": Vector()
+    }))
     balls.append(ball)
 
 player = rb.Sprite({
@@ -65,23 +70,25 @@ player = rb.Sprite({
 })
 
 player_rb = rb.RigidBody({
-    "mass": 5,
-    "bouncyness": 0.1,
+    "mass": 10,
+    "bounciness": 1,
     "max_speed": Vector(50, 1000),
     "gravity": Vector()
 })
 player.add_component(player_rb)
 
-player_hitbox = rb.Polygon.generate_rect(32, 32)
+player_hitbox = rb.Polygon({
+    "verts": rb.Polygon.generate_rect(32, 32),
+    "color": Color.blue,
+})
 player.add_component(player_hitbox)
-player_hitbox.debug = True
 
 
 def custom_update():
     if rb.Input.is_pressed("w"):
-        player_rb.velocity.y = -200
+        player_rb.velocity.y -= 100
     elif rb.Input.is_pressed("s"):
-        player_rb.velocity.y = 200
+        player_rb.velocity.y += 100
     if rb.Input.is_pressed("a"):
         player_rb.velocity.x -= 50
     elif rb.Input.is_pressed("d"):
