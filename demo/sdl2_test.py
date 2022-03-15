55647a34d64016c9d0e33e284923176740b27bf9
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2022-03-15 16:54:06 -0400

    sdl test

--- demo/sdl2_test.py
+++ demo/sdl2_test.py
@@ -4,11 +4,14 @@ import sdl2.ext
 import sdl2.surface
 import sdl2.sdlimage
 import sdl2.sdlgfx
+import time
 
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../rubato"))
+
+from rubato.classes.components.image import Image
 
 sdl2.SDL_Init(sdl2.SDL_INIT_EVERYTHING)
 
@@ -17,11 +20,20 @@ window.show()
 
 sdl2.ext.fill(window.get_surface(), (255, 255, 255))
 
-render = sdl2.ext.Renderer(window, backend="direct3d")
+render = sdl2.ext.Renderer(window,
+                           flags=(sdl2.SDL_RENDERER_ACCELERATED
+                                  | sdl2.SDL_RENDERER_PRESENTVSYNC))
 
-sdl2.sdlgfx.aacircleColor(render.sdlrenderer, 50, 50, 25, 0x000000FF)
+render.fill((0, 0, render.logical_size[0], render.logical_size[1]), 0xFFFFFFFF)
 
+sdl2.sdlgfx.aacircleRGBA(render.sdlrenderer, 50, 50, 25, 0, 0, 0, 255)
+sdl2.sdlgfx.filledCircleRGBA(render.sdlrenderer, 50, 50, 25, 0, 0, 0, 255)
+
+img = Image({"image_location": os.path.abspath("demo/testing/Death/0.png")})
+
+render.copy(sdl2.ext.Texture(render.sdlrenderer, img.image),
+            dstrect=(100, 100))
+
+window.refresh()
 render.present()
-
-processor = sdl2.ext.TestEventProcessor()
-processor.run(window)
+time.sleep(5)
