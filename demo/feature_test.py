a77b8c6e324a586acdaa2f4caaf6507057a03109
Tomer Sedan <tomer.sedan@protonmail.com>
2022-03-15 23:58:47 -0400

    working base functionality

--- demo/feature_test.py
+++ demo/feature_test.py
@@ -7,9 +7,16 @@ sys.path.insert(0, os.path.abspath("../"))
 
 import rubato as rb
 
+
 rb.init()
 
 main = rb.Scene()
 rb.Game.scenes.add(main, "main")
+
+main.add(rb.Sprite({
+    "pos": rb.Vector(100,100)
+}).add(rb.Circle({
+    "color": rb.Color.green
+})))
 
 rb.begin()
