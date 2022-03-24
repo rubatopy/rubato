8655c25e191b2d8e356177dac2ca46167d0a7845
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2022-03-24 17:16:01 -0400

    Format files

--- demo/ball_pit.py
+++ demo/ball_pit.py
@@ -8,81 +8,127 @@ from random import randint, choice
 import rubato as rb
 from rubato import Game, Vector, Color, Display
 
-rb.init({
-    "name": "Physics Demo",
-    "window_size": Vector(600, 600),
-    "res": Vector(1200, 1200),
-})
+rb.init(
+    {
+        "name": "Physics Demo",
+        "window_size": Vector(600, 600),
+        "res": Vector(1200, 1200),
+    }
+)
 
 main_scene = rb.Scene()
 Game.scenes.add(main_scene, "main")
 
 top = rb.Sprite({
     "pos": Display.top_center + Vector(0, -30)
-}).add(rb.Rectangle({
-    "width": Display.res.x + 175,
-    "height": Display.res.y / 10,
-    "color": Color.gray,
-}))
+}).add(
+    rb.Rectangle(
+        {
+            "width": Display.res.x + 175,
+            "height": Display.res.y / 10,
+            "color": Color.gray,
+        }
+    )
+)
 
 bottom = rb.Sprite({
     "pos": Display.bottom_center + Vector(0, 30)
-}).add(rb.Rectangle({
-    "width": Display.res.x + 175,
-    "height": Display.res.y / 10,
-    "color": Color.gray,
-}))
+}).add(
+    rb.Rectangle(
+        {
+            "width": Display.res.x + 175,
+            "height": Display.res.y / 10,
+            "color": Color.gray,
+        }
+    )
+)
 
 left = rb.Sprite({
     "pos": Display.center_left + Vector(-30, 0)
-}).add(rb.Rectangle({
-    "width": Display.res.x / 10,
-    "height": Display.res.y + 175,
-    "color": Color.gray,
-}))
+}).add(
+    rb.Rectangle(
+        {
+            "width": Display.res.x / 10,
+            "height": Display.res.y + 175,
+            "color": Color.gray,
+        }
+    )
+)
 
 right = rb.Sprite({
     "pos": Display.center_right + Vector(30, 0)
-}).add(rb.Rectangle({
-    "width": Display.res.x / 10,
-    "height": Display.res.y + 175,
-    "color": Color.gray,
-}))
+}).add(
+    rb.Rectangle(
+        {
+            "width": Display.res.x / 10,
+            "height": Display.res.y + 175,
+            "color": Color.gray,
+        }
+    )
+)
 
 for _ in range(60):
     main_scene.add(
-        rb.Sprite({
-            "pos":
-                Vector(randint(Display.res.x / 20, 19 * Display.res.x / 20),
-                       randint(Display.res.y / 20, 19 * Display.res.y / 20))
-        }).add(
-            rb.Circle({
-                "radius": Display.res.x / 50,
-                "color": Color(*choice(list(rb.Defaults.color_defaults.values())))
-            })).add(
-                rb.RigidBody({
+        rb.Sprite(
+            {
+                "pos":
+                    Vector(
+                        randint(Display.res.x / 20, 19 * Display.res.x / 20),
+                        randint(Display.res.y / 20, 19 * Display.res.y / 20)
+                    )
+            }
+        ).add(
+            rb.Circle(
+                {
+                    "radius":
+                        Display.res.x / 50,
+                    "color":
+                        Color(
+                            *choice(list(rb.Defaults.color_defaults.values()))
+                        )
+                }
+            )
+        ).add(
+            rb.RigidBody(
+                {
                     "bounciness": 1,
                     "friction": 0.2,
                     "gravity": Vector(0, Display.res.x / 8),
                     "velocity": Vector(randint(-100, 100), randint(-100, 100))
-                })))
+                }
+            )
+        )
+    )
 
 player = rb.Sprite({
     "pos": rb.Vector(50, 50),
     "debug": True
-}).add(rb.Image({
-    "image_location": "testing/Idle/0.png",
-    "scale_factor": rb.Vector(2, 2)
-}))
+}).add(
+    rb.Image(
+        {
+            "image_location": "testing/Idle/0.png",
+            "scale_factor": rb.Vector(2, 2)
+        }
+    )
+)
 
-player_rb = rb.RigidBody({"mass": 0, "bounciness": 0.1, "max_speed": Vector(1000, 1000), "gravity": Vector()})
+player_rb = rb.RigidBody(
+    {
+        "mass": 0,
+        "bounciness": 0.1,
+        "max_speed": Vector(1000, 1000),
+        "gravity": Vector()
+    }
+)
 player.add(player_rb)
 
-player_hitbox = rb.Rectangle({
-    "width": Display.res.x / 16,
-    "height": Display.res.x / 16,
-    "debug": True,
-})
+player_hitbox = rb.Rectangle(
+    {
+        "width": Display.res.x / 16,
+        "height": Display.res.x / 16,
+        "debug": True,
+    }
+)
 player.add(player_hitbox)
 
 
@@ -114,6 +160,7 @@ def custom_update():
 
     if rb.Input.key_pressed("a", "Shift"):
         print("WOW the secret combo was pressed.")
+
 
 main_scene.add(top, bottom, left, right, player)
 main_scene.update = custom_update
