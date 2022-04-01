b5ed742a51067337001fc189e1705f1638ce38fc
Yamm Elnekave <klavlavigugu@gmail.com>
2022-03-31 17:27:46 -0700

    one line :cry:

--- demo/text_demo.py
+++ demo/text_demo.py
@@ -12,7 +12,7 @@ import sdl2.ext
 main = rb.Scene()
 rb.Game.scenes.add(main, "main")
 image = rb.Image({"rel_path": "testing/Run/0.png"})
-image.resize(rb.Vector(200, 200))
+# image.resize(rb.Vector(200, 200))
 ttf.TTF_Init()
 
 fontfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testing/fonts", "32bit Regular.ttf").encode("utf-8")
@@ -22,7 +22,7 @@ font = ttf.TTF_OpenFont(fontfile, 64).contents
 #                                 ttf.TTF_STYLE_ITALIC)
 text_color = sdl2.SDL_Color(30, 30, 20, 200)
 surface = ttf.TTF_RenderText_Solid(font, b"Hello World!", text_color).contents
-main.draw = lambda: rb.Display.update(sdl2.ext.Texture(rb.Display.renderer, surface), rb.Vector(100, 100))
+# main.draw = lambda: rb.Display.update(sdl2.ext.Texture(rb.Display.renderer, surface), rb.Vector(100, 100))
 image.image = surface
 print(surface.__class__)
 text = rb.GameObject({"pos": rb.Vector(200, 200)}).add(image)
