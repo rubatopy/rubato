10b6abc3ef4fb7db8e6cf74e29535a033aad0d39
Yamm Elnekave <klavlavigugu@gmail.com>
2022-04-01 20:43:58 -0700

    step 2 fixed

--- tutorials/platformer/step2/main.py
+++ tutorials/platformer/step2/main.py
@@ -1,26 +1,26 @@
 """
 Step 2 of the Platformer Tutorial.
 """
-import rubato as rb
+from rubato import *
 
 # Initialize a new game
-rb.init(
+init(
     {
         "name": "Platformer Demo",  # Set a name
-        "window_size": rb.Vector(960, 540),  # Set the window size
-        "background_color": rb.Color.cyan.lighter(),  # Set the background color
-        "res": rb.Vector(1920, 1080),  # Increase the window resolution
+        "window_size": Vector(960, 540),  # Set the window size
+        "background_color": Color.cyan.lighter(),  # Set the background color
+        "res": Vector(1920, 1080),  # Increase the window resolution
     }
 )
 
 # Create a scene
-main = rb.Scene()
+main = Scene()
 # Add the scene to the scene manager and give it a name
-rb.Game.scenes.add(main, "main")
+Game.scenes.add(main, "main")
 
 # Create the player
-player = rb.GameObject({
-    "pos": rb.Display.center_left + rb.Vector(50, 0),  # Set the starting position of the player
+player = GameObject({
+    "pos": Display.center_left + Vector(50, 0),  # Set the starting position of the player
 })
 dino_image = Image({"rel_path": "sprites/dino/shadow.png", "scale_factor": Vector(4, 4)})
 
@@ -29,4 +29,4 @@ dino_image = Image({"rel_path": "sprites/dino/shadow.png", "scale_factor": Vecto
 main.add(player)
 
 # begin the game
-rb.begin()
+begin()
