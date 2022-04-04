f73300ca0c64e8756e628305bd64227ebf5fe90e
Yamm Elnekave <klavlavigugu@gmail.com>
2022-04-03 17:04:54 -0700

    finished noise
    resolves #68

--- demo/test_noise.py
+++ demo/test_noise.py
@@ -11,7 +11,7 @@ rb.init({
 main_scene = rb.Scene()
 rb.Game.scenes.add(main_scene, "main")
 
-scale = 20
+scale = 50
 offset = rb.Vector(100, 100)
 
 saved = []
@@ -19,8 +19,8 @@ for x in range(rb.Display.res.x):
     saved.append([])
     for y in range(rb.Display.res.y):
         noise = opensimplex.noise2((x + offset.x) / scale, (y + offset.y) / scale)
-        gray = (noise + 1) / 2 * 255  # Note simplex perlin noise ranges from -1 to 1
-        color = (gray, gray, gray)
+        gray = (noise + 1) / 2 * 255  # Note simplex perlin noise ranges from -1 to 1 and is being scaled to 0-255
+        color = [gray for i in range(3)]
         saved[x].append(([x, y], color))
         rb.Display.renderer.draw_point([x, y], color)
 
@@ -28,6 +28,7 @@ def draw():
     for i in range(rb.Display.res.x):
         for j in range(rb.Display.res.y):
             rb.Display.renderer.draw_point(*saved[i][j])
+
 
 main_scene.draw = draw
 
