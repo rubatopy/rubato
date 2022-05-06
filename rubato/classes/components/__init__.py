7b8b642cfb047404783e916b083d451217ddeb5f
Yamm Elnekave <klavlavigugu@gmail.com>
2022-05-06 14:20:26 -0700

    start slider.

--- rubato/classes/components/__init__.py
+++ rubato/classes/components/__init__.py
@@ -6,6 +6,7 @@ from .hitbox import Hitbox, Polygon, Circle, Rectangle
 from .rigidbody import RigidBody
 from .text import Text
 from .button import Button
+from .slider import Slider
 from .image import Image
 from .animation import Animation
 from .spritesheet import Spritesheet
