90679da8bec04b0317f0a4ad8edb976483efe70f
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2022-05-21 10:23:22 -0700

    Fully test init
    Progress on #121

--- rubato/tests/test_init.py
+++ rubato/tests/test_init.py
@@ -1,7 +1,9 @@
 """Test the initialization of Rubato."""
+from importlib.resources import files
 from unittest.mock import Mock
 import pytest
 import rubato
+import sdl2
 
 
 @pytest.mark.rub
@@ -31,7 +33,49 @@ def test_begin_init(monkeypatch, rub):
     loop.assert_called_once()  # This code is reachable because of the monkeypatch
 
 
-# @pytest.mark.rub
-# def test_init(monkeypatch):
-#     assert rubato.Game.initialized is False
-#     rubato.init({"target_fps": 0})
+@pytest.mark.rub
+def test_init(monkeypatch):
+    set_icon = Mock()
+    monkeypatch.setattr(rubato.Display, "set_window_icon", set_icon)
+
+    assert rubato.Game.initialized is False
+    rubato.init(
+        {
+            "name": "Untitled Game",
+            "window_size": rubato.Vector(360, 360),
+            "res": rubato.Vector(1080, 1080),
+            "target_fps": 60,
+            "physics_fps": 30,
+            "border_color": rubato.Color(0, 0, 0),
+            "background_color": rubato.Color(255, 255, 255),
+            "icon": "",
+            "hidden": False,
+        }
+    )
+    assert rubato.Game.initialized is True
+    assert rubato.Game.state == rubato.Game.STOPPED
+    assert rubato.Game.border_color == rubato.Color(0, 0, 0)
+    assert rubato.Game.background_color == rubato.Color(255, 255, 255)
+
+    assert rubato.Time.target_fps == 60
+    assert rubato.Time.capped
+    assert rubato.Time.normal_delta == 1000 / 60
+    assert rubato.Time.physics_fps == 30
+
+    set_icon.assert_called_once_with(files("rubato.static.png").joinpath("logo_filled.png"))
+    set_icon.reset_mock()
+    sdl2.SDL_Quit()
+    rubato.init(
+        {
+            "name": "Untitled Game",
+            "window_size": rubato.Vector(360, 360),
+            "res": rubato.Vector(1080, 1080),
+            "target_fps": 60,
+            "physics_fps": 30,
+            "border_color": rubato.Color(0, 0, 0),
+            "background_color": rubato.Color(255, 255, 255),
+            "icon": files("rubato.static.png").joinpath("logo_filled.ico"),
+            "hidden": True,
+        }
+    )
+    set_icon.assert_called_once_with(files("rubato.static.png").joinpath("logo_filled.ico"))
