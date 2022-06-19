23ab6f18f828f7539e14875813037a0ad47e3253
Yamm Elnekave <52299477+mrElnekave@users.noreply.github.com>
2022-06-18 23:23:46 -0700

    bringing back find my print.

--- rubato/utils/debug.py
+++ rubato/utils/debug.py
@@ -1,7 +1,7 @@
 """
 This Debug module provides a set of functions to help with debugging.
 """
-import sys
+import sys, traceback
 
 from . import PrintError
 
@@ -10,6 +10,29 @@ class Debug:
     """
     Debug comes with useful functions to help with debugging.
     """
+
+    @staticmethod
+    def find_my_print():
+        """
+        Will print the stack when it finds a print statement.
+        Examples:
+            Place this next to your rubato import.
+            >>> Debug.find_my_print()
+        """
+
+        class TracePrints(object):
+
+            def __init__(self):
+                self.stdout = sys.stdout
+
+            def write(self, _):
+                traceback.print_stack(file=self.stdout)
+
+            def flush(self):
+                self.stdout.flush()
+
+        sys.stdout = TracePrints()
+
     @staticmethod
     def error_my_print():
         """
