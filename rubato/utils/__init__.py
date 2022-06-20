9a3a07c5c0c8ace7d0a365b5dba09491fc0d34f5
Tomer Sedan <tomer.sedan@pm.me>
2022-06-20 12:11:43 -0700

    clean game.py

--- rubato/utils/__init__.py
+++ rubato/utils/__init__.py
@@ -11,8 +11,8 @@ from .color import Color
 from .font import Font
 from .draw import Draw, DrawTask
 from .rb_time import Time
-from .radio import Radio, Events
 from .rb_input import Input
 from .sound import Sound
 from .quickselect import quickselect
 from .debug import Debug
+from .radio import Radio, Events
