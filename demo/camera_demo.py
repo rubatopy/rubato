188d17ab2cf06c630c27c8e718e2855d4692bf3d
Yamm Elnekave <52299477+mrElnekave@users.noreply.github.com>
2022-06-29 13:14:53 -0700

    set and translate functionality

---
+++ demo/camera_demo.py
@@ -0,0 +1,24 @@
+"""Camera demo"""
+from rubato import *
+
+init(res=Vector(500, 500))
+
+main_scene = Scene(name="main")
+
+player = GameObject(pos=Display.center)
+player.add(Rectangle(width=50, height=50, color=Color.red))
+
+Radio.listen("")
+def update():
+    if Input.mouse_pressed():
+        pos = main_scene.camera.transform(Input.get_mouse_pos())
+        print(pos)
+        dot = GameObject(pos=pos).add(Circle(radius=5, color=Color.blue))
+        main_scene.add(dot)
+        main_scene.camera.set(pos)
+        # main_scene.camera
+
+main_scene.add(player)
+main_scene.update = update
+
+begin()
