b472438a44e6812a190a9724fc53122e0a033945
Tomer Sedan <tomer.sedan@pm.me>
2022-07-06 23:01:47 -0700

    wrap component array

--- rubato/misc.py
+++ rubato/misc.py
@@ -1,9 +1,10 @@
 """Miscellaneous helper functions for rubato developers."""
+from typing import List
 
 from . import Vector, GameObject, Component
 
 def wrap(
-    comp: Component,
+    comp: Component | List[Component],
     name: str = "",
     pos: Vector = Vector(),
     rotation: float = 0,
@@ -11,4 +12,12 @@ def wrap(
     debug: bool = False
 ):
     """Automatically creates a gameobject surrounding a component and returns it."""
-    return GameObject(name=name, pos=pos, rotation=rotation, z_index=z_index, debug=debug).add(comp)
+    go = GameObject(name=name, pos=pos, rotation=rotation, z_index=z_index, debug=debug)
+    if isinstance(comp, Component):
+        go.add(comp)
+    elif isinstance(comp, list):
+        for c in comp:
+            go.add(c)
+    else:
+        raise TypeError("comp must be a Component or a list of Components.")
+    return go
