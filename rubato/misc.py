862d12478877b35c8f1e4450851bd150f0162223
Martin Chaperot <martin.chapino@gmail.com>
2022-07-06 23:13:07 -0700

    Add misc to docs

--- rubato/misc.py
+++ rubato/misc.py
@@ -3,6 +3,7 @@ from typing import List
 
 from . import Vector, GameObject, Component
 
+
 def wrap(
     comp: Component | List[Component],
     name: str = "",
@@ -15,18 +16,18 @@ def wrap(
     Wraps a component or list of components in a GameObject.
 
     Args:
-        comp (Component | List[Component]): The component or list of components to wrap.
-        name (str, optional): The name of the GameObject. Defaults to "".
-        pos (Vector, optional): The position of the GameObject. Defaults to Vector().
-        rotation (float, optional): The rotation of the GameObject. Defaults to 0.
-        z_index (int, optional): The z_index of the GameObject. Defaults to 0.
-        debug (bool, optional): Whether the GameObject is in debug mode. Defaults to False.
+        comp: The component or list of components to wrap.
+        name: The name of the GameObject. Defaults to "".
+        pos: The position of the GameObject. Defaults to Vector().
+        rotation: The rotation of the GameObject. Defaults to 0.
+        z_index: The z_index of the GameObject. Defaults to 0.
+        debug: Whether the GameObject is in debug mode. Defaults to False.
 
     Raises:
         TypeError: If comp is not a Component or a list of Components.
 
     Returns:
-        GameObject: The wrapped GameObject.
+        The wrapped GameObject.
     """
     go = GameObject(name=name, pos=pos, rotation=rotation, z_index=z_index, debug=debug)
     if isinstance(comp, Component):
