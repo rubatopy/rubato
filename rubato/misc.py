e932d496cf3adb0c544ed7bff12344d88a55fc17
Tomer Sedan <tomer.sedan@pm.me>
2022-07-06 23:06:24 -0700

    Add goog docstring

--- rubato/misc.py
+++ rubato/misc.py
@@ -11,7 +11,23 @@ def wrap(
     z_index: int = 0,
     debug: bool = False
 ):
-    """Automatically creates a gameobject surrounding a component and returns it."""
+    """
+    Wraps a component or list of components in a GameObject.
+
+    Args:
+        comp (Component | List[Component]): The component or list of components to wrap.
+        name (str, optional): The name of the GameObject. Defaults to "".
+        pos (Vector, optional): The position of the GameObject. Defaults to Vector().
+        rotation (float, optional): The rotation of the GameObject. Defaults to 0.
+        z_index (int, optional): The z_index of the GameObject. Defaults to 0.
+        debug (bool, optional): Whether the GameObject is in debug mode. Defaults to False.
+
+    Raises:
+        TypeError: If comp is not a Component or a list of Components.
+
+    Returns:
+        GameObject: The wrapped GameObject.
+    """
     go = GameObject(name=name, pos=pos, rotation=rotation, z_index=z_index, debug=debug)
     if isinstance(comp, Component):
         go.add(comp)
