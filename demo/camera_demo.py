4d79a0c0174bbbd3092bbe05f79c71e1b9130cdc
Tomer Sedan <tomer.sedan@pm.me>
2022-07-10 14:26:39 -0700

    add world_mouse

--- demo/camera_demo.py
+++ demo/camera_demo.py
@@ -3,20 +3,19 @@ from rubato import *
 
 init(res=Vector(500, 500))
 
-main_scene = Scene(name="main")
+main = Scene(name="main")
 
 player = GameObject(pos=Display.center)
 player.add(Rectangle(width=50, height=50, color=Color.red))
 
 
 def mouse_down(_):
-    pos = Game.camera.i_transform(Input.get_mouse_pos())
-    dot = GameObject(pos=pos).add(Circle(radius=5, color=Color.blue))
-    main_scene.add(dot)
-    main_scene.camera.pos = pos
+    main.camera.pos += Vector(5, -10)
+    main.add(GameObject(pos=world_mouse()).add(Circle(radius=5, color=Color.blue)))
+
 Radio.listen(Events.MOUSEDOWN, mouse_down)
 
 
-main_scene.add(player)
+main.add(player)
 
 begin()
