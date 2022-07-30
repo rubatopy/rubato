2af330e9fda62bb8cb4f139bc46e4cc16134930a
Martin Chaperot <martin.chapino@gmail.com>
2022-07-29 17:34:56 -0700

    caching things good

--- docs/v_sidebar_saver.py
+++ docs/v_sidebar_saver.py
@@ -1,8 +1,12 @@
 # A script that caches the sidebar of a page and saves it to a file.
+# Must be run from the docs
 import requests
 
 r = requests.get("https://test.rubato.app/version_iframe_generator.html")
 
-with open("version_iframe.html", "w") as f:
-    f.write(r.text)
-    
+import urllib.request
+with urllib.request.urlopen('http://python.org/') as response:
+    html = response.read()
+
+with open("version_iframe.html", "w", encoding="utf-8") as f:
+    f.write(html.decode("utf-8"))
