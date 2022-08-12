e79dabee0e912949f5201591ceb49a0ca1c20597
Martin Chaperot <martin.chapino@gmail.com>
2022-08-11 21:08:58 -0700

    remove sdl and change include

--- demo/test/PixelEditor.cpp
+++ demo/test/PixelEditor.cpp
@@ -1,8 +1,9 @@
-#include "SDL2/include/SDL.h"
+#include <cstdint>
+typedef uint32_t Uint32;
 
-
-inline void setPixelRGB(size_t _pixels, int width, int x, int y, int mapped) {
-	Uint32* pixels = (Uint32*)(_pixels);
-	Uint32* pixel = pixels + (y*width) + x;
-	*pixel = (Uint32) mapped;
+inline void setPixelRGB(size_t _pixels, int width, int x, int y, int mapped)
+{
+    Uint32 *pixels = (Uint32 *)(_pixels);
+    Uint32 *pixel = pixels + (y * width) + x;
+    *pixel = (Uint32)mapped;
 }
