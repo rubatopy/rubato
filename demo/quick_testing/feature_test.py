03b43932b5b2c8bec903c6541f95dc9f58e691ad
Tomer Sedan <tomer.sedan@pm.me>
2022-08-19 13:44:49 -0700

    ms paint

--- demo/quick_testing/feature_test.py
+++ demo/quick_testing/feature_test.py
@@ -1,27 +1,27 @@
 """A place to test new WIP features"""  # pylint: disable=all
 import rubato as rb
 from rubato import Vector as V
-import sdl2, sdl2.ext
 
 width, height = 256, 256
+off = int(width / 8)
 
-rb.init(res=V(width, height), window_size=V(width, height) * 2)
+rb.init(res=V(width, height), window_size=V(width, height) * 20)
 s = rb.Scene()
 
 raster = rb.Raster(width, height, offset=V(width / 2, height / 2))
-img = raster.surf
-
-img.clear()
-
-img.draw_line(V(4, 3), V(28, 27), rb.Color.blue, aa=True, thickness=3, blending=True)
+raster2 = rb.Raster(width, height, offset=V(width / 2, height / 2))
 
 
 def update():
+    raster.surf.clear()
+    raster.surf.draw_poly(
+        [v + rb.world_mouse().floor() for v in rb.Vector.poly(8, off)], rb.Color.blue, aa=True, blending=True
+    )
     if rb.Input.mouse_pressed():
-        img.draw_point(rb.Input.get_mouse_pos(), rb.Color(32, 32, 32))
+        raster2.merge(raster)
 
 
-s.add(rb.wrap(raster))
+s.add(rb.wrap([raster2, raster]))
 rb.Game.update = update
 
 rb.begin()
