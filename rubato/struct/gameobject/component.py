825395c0f2bce656b68a5c185562501651caaff5
Tomer Sedan <tomer.sedan@pm.me>
2022-08-26 15:13:11 -0400

    clean true_pos and true_rotation

--- rubato/struct/gameobject/component.py
+++ rubato/struct/gameobject/component.py
@@ -56,13 +56,13 @@ class Component:
 
     def true_pos(self) -> Vector:
         """Returns the world position of the component."""
-        return (self.gameobj.pos if self.gameobj else 0) + self.offset.rotate(
-            (self.gameobj.rotation if self.gameobj else 0)
-        )
+        if self.gameobj:
+            return self.gameobj.pos + self.offset.rotate(self.gameobj.rotation)
+        return self.offset
 
     def true_rotation(self) -> float:
         """Returns the rotation of the component offset by its parent gameobject rotation."""
-        return (self.gameobj.rotation if self.gameobj else 0) + self.rot_offset
+        return self.rot_offset + (self.gameobj.rotation if self.gameobj else 0)
 
     def _setup(self):
         self.started = True
