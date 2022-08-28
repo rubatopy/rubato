947353dbe92997ecaab0609d0912df3074f863f5
Yamm Elnekave <klavlavigugu@gmail.com>
2022-08-27 22:20:23 -0800

    check for setting alpha of shapes.

--- demo/quick_testing/UI_test.py
+++ demo/quick_testing/UI_test.py
@@ -10,8 +10,12 @@ main = Scene()
 
 txt = wrap(Text("Hello World!", Font(size=64), rot_offset=25), pos=Display.center + 100, rotation=45)
 img = Image("../sprites/spaceship/spaceship.png")
+rast = Raster(32, 32)
+rast.draw_rect((0, 0), (32, 32), Color.red)
 
-main.add(txt, wrap(img, pos=Display.center))
+rast.set_alpha(30)
+
+main.add(txt, wrap(img, pos=Display.center), wrap(rast, pos=Display.center - 100))
 
 Game.draw = lambda: Draw.rect(Display.center, 100, 100, Color.red)
 
