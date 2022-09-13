3d4620dff6073be7044e9c6c60e51c001c698b28
Tomer Sedan <tomer.sedan@pm.me>
2022-09-13 13:27:30 -0400

    add psutil overall mem useage counter

--- demo/memory_test.py
+++ demo/memory_test.py
@@ -1,14 +1,21 @@
 from rubato import *
 from pympler import tracker
-import os
+import psutil, os
 
 tr = tracker.SummaryTracker()
 
 init()
 
 
+def process_memory():
+    process = psutil.Process(os.getpid())
+    mem_info = process.memory_info()
+    return mem_info.rss
+
+
 def print_mem():
     os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"Overall useage: {process_memory()}\n")
     tr.print_diff()
 
 
