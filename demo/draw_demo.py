8e5ef8562cfcdf3aa016f62523dc78e535931d92
Tomer Sedan <tomer.sedan@pm.me>
2022-09-29 13:57:15 -0400

    update draw_demo

--- demo/draw_demo.py
+++ demo/draw_demo.py
@@ -1,12 +1,14 @@
 """Draw Demo"""  # pylint: disable=all
 import rubato as rb
 
-rb.init(res=(32 * 3, 32 * 2), window_size=(1000, 1000))
+rb.init(res=(96, 64), window_size=(960, 640))
 
 c = rb.Camera()
 
+rb.Game.show_fps = True
 
-def update():
+
+def draw():
     rb.Draw.queue_point((16, 16), camera=c)
     rb.Draw.queue_line((32, 0), (64, 31), camera=c)
     rb.Draw.queue_rect((64 + 16, 16), 32, 32, fill=rb.Color.red, camera=c)
@@ -14,6 +16,6 @@ def update():
     rb.Draw.queue_poly([v + rb.Vector(32 + 16, 32 + 16) for v in rb.Vector.poly(5, 16)], fill=rb.Color.red, camera=c)
 
 
-rb.Game.update = update
+rb.Game.draw = draw
 
 rb.begin()
