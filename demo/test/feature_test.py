0236b1657fea8cd9b592397ca095eec35e008d5b
Martin Chaperot <martin.chapino@gmail.com>
2022-11-07 23:08:13 -0500

    make sure ui works

--- demo/test/feature_test.py
+++ demo/test/feature_test.py
@@ -1,20 +1,41 @@
 """A place to test new WIP features"""
 import rubato as rb
 
+rb.init(res=(1080, 1080), window_size=(2000, 2000))
 
-class A:
-
-    def replacement(self):
-        """wow"""
-        pass
+main = rb.Scene()
 
 
-@rb.deprecated("1.0.0", "2.0.0", A.replacement)
-def test():
-    """wow"""
-    pass
+def on_click():
+    print("Button clicked!")
 
 
-print(test.__doc__)
-test()
-print("next thing")
+def on_release():
+    print("Button released!")
+
+
+def on_hover():
+    print("Mouse entered button!")
+
+
+def on_exit():
+    print("Mouse exited button!")
+
+
+button = rb.Button(
+    50,
+    50,
+    on_click,
+    on_release,
+    on_hover,
+    on_exit,
+)
+
+rect = rb.Raster(50, 50)
+rect.fill(rb.Color(255, 0, 0))
+
+go = rb.GameObject()
+go.add(button, rect)
+
+main.add(go)
+rb.begin()
