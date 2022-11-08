4613c7780150a99ea7717d5bcba73f8af84ccbef
Martin Chaperot <martin.chapino@gmail.com>
2022-11-08 00:23:15 -0500

    platformer rewrite

--- rubato/__pyinstaller/hook-rubato.py
+++ rubato/__pyinstaller/hook-rubato.py
@@ -1,4 +1,5 @@
 """The PyInstaller hooks file for rubato."""  # pylint: disable=invalid-name
 from PyInstaller.utils.hooks import collect_data_files
 
-datas = [*collect_data_files("rubato"), *collect_data_files("sdl2dll")]
+datas = collect_data_files("rubato")
+datas += collect_data_files("sdl2dll")
