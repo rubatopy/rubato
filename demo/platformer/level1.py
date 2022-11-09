aa06afceef500efd7536fae0630e5bfc1728dda2
Martin Chaperot <martin.chapino@gmail.com>
2022-11-09 16:03:37 -0500

    level 1 pretty much done

--- demo/platformer/level1.py
+++ demo/platformer/level1.py
@@ -1,9 +1,11 @@
-from rubato import Scene, Color, Display, GameObject, Vector, Rectangle, wrap, Font, Text
+from rubato import Scene, Color, Display, GameObject, Vector, Rectangle, wrap, Font, Text, Radio, Events, Game, Time
+import shared
+from data_scene import DataScene
 
-level_size = int(Display.res.x * 1.2)
-portal_location = Vector(Display.left + level_size - 128, 450)
+scene = DataScene("level1", background_color=Color.cyan.lighter())
+scene.level_size = int(Display.res.x * 1.2)
 
-scene = Scene("level1", background_color=Color.cyan.lighter())
+portal_location = Vector(Display.left + scene.level_size - 128, 450)
 
 # create the ground
 ground = GameObject().add(ground_rect := Rectangle(width=1270, height=50, color=Color.green.darker(40), tag="ground"))
@@ -51,12 +53,34 @@ for pillar in pillars:
     r.tag = "ground"
     r.color = Color.purple
 
-win_font = Font(size=128, color=Color.green)
-win_text = GameObject().add(Text("YOU WIN!", win_font))
+# level win screen
+win_font = Font(size=128, color=Color.green.darker(75), styles=["bold"])
+win_text = GameObject(z_index=2).add(Text("YOU WIN!", win_font, anchor=(0, 0.5)))
+win_sub_text = GameObject(pos=(0, -100), z_index=2).add(Text("Click anywhere to move on", shared.black_32))
+
+has_won = False
 
 
 def won():
-    pass
+    global click_listener, has_won
+    if not has_won:
+        has_won = True
+        click_listener = Radio.listen(Events.MOUSEDOWN, go_to_next)
+        scene.add_ui(win_text, win_sub_text)
 
 
-scene.add(ground, wrap(platforms), *pillars)
+def go_to_next():
+    Game.set_scene("level2")
+    click_listener.remove()
+
+
+def switch():
+    shared.player.pos = Display.bottom_left + Vector(50, 50)
+    shared.player_comp.initial_pos = shared.player.pos.clone()
+    shared.right.pos = Display.center_left + Vector(scene.level_size + 25, 0)
+    shared.portal.pos = portal_location
+    shared.portal.get(Rectangle).on_enter = lambda col_info: won() if col_info.shape_b.tag == "player" else None
+
+
+scene.on_switch = switch
+scene.add(ground, wrap(platforms), *pillars, shared.player, shared.left, shared.right, shared.portal)
--- demo/platformer/main.py
+++ demo/platformer/main.py
@@ -12,7 +12,6 @@ rb.init(
     window_size=(2880, 1620),  # Set the window size
 )
 
-from player import Player
 import main_menu
 import level1
 import level2
@@ -22,114 +21,9 @@ import level2
 # rb.Game.show_fps = True
 # rb.Game.current().camera.zoom = 1
 
-current_scene = main_menu
 rb.Game.set_scene("main_menu")
 
-##### PLAYER #####
-
-# Create the player and set its starting position
-player = rb.GameObject(
-    pos=rb.Display.bottom_left + rb.Vector(50, 50),
-    z_index=1,
-)
-
-# Create animation and initialize states
-p_animation = rb.Spritesheet.from_folder(
-    path="files/dino",
-    sprite_size=rb.Vector(24, 24),
-    default_state="idle",
-)
-p_animation.scale = rb.Vector(4, 4)
-p_animation.fps = 10  # The frames will change 10 times a second
-player.add(p_animation)  # Add the animation component to the player
-
-player.add(
-    # add a hitbox to the player with the collider
-    rb.Rectangle(width=40, height=64, tag="player"),
-    # add a ground detector
-    rb.Rectangle(
-        width=34,
-        height=2,
-        offset=rb.Vector(0, -32),
-        trigger=True,
-        tag="player_ground_detector",
-    ),
-    # add a rigidbody to the player
-    rb.RigidBody(gravity=rb.Vector(y=rb.Display.res.y * -1.5), pos_correction=1),
-    # add custom player component
-    Player(),
-)
-
-##### SHARED LEVEL ITEMS #####
-
-# Create animation for portal
-all_portal_images = rb.Spritesheet(
-    path="files/portals/portal1_spritesheet.png",
-    sprite_size=rb.Vector(32, 32),
-    grid_size=rb.Vector(8, 1),
-)
-
-portal_animation = rb.Animation(scale=rb.Vector(4, 4), fps=10)
-portal_animation.add_spritesheet("", all_portal_images, to_coord=all_portal_images.end)
-
-# create the end portal
-portal = rb.GameObject(pos=current_scene.portal_location)
-portal.add(portal_animation)
-
-portal.add(
-    rb.Rectangle(
-        trigger=True,
-        tag="portal",
-        width=portal_animation.anim_frame().size_scaled().x,
-        height=portal_animation.anim_frame().size_scaled().y,
-        on_collide=lambda col_info: current_scene.won() if col_info.shape_b.tag == "player" else None,
-    )
-)
-
-# Side boundary
-left = rb.GameObject(pos=rb.Display.center_left - rb.Vector(25, 0)).add(rb.Rectangle(width=50, height=rb.Display.res.y))
-right = rb.GameObject().add(rb.Rectangle(width=50, height=rb.Display.res.y))
-
 ##### SCENE SETUP #####
-
-level1.scene.add(left, right, player, portal)
-
-
-def level_switch():
-    right.pos = rb.Display.center_left + rb.Vector(current_scene.level_size + 25, 0)
-    portal.pos = current_scene.portal_location
-
-
-def switch_to_level1():
-    global current_scene
-    current_scene = level1
-    rb.Game.set_scene("level1")
-    level_switch()
-
-
-def switch_to_level2():
-    global current_scene
-    current_scene = level2
-    rb.Game.set_scene("level2")
-    level_switch()
-
-
-main_menu.play_button.get(rb.Button).onclick = switch_to_level1
-
-
-# define a custom fixed update function
-def fixed_update():
-    # have the camera follow the player
-    camera_ideal = rb.Math.clamp(
-        player.pos.x + rb.Display.res.x / 4, rb.Display.center.x, current_scene.level_size - rb.Display.res.x
-    )
-    rb.Game.current().camera.pos.x = rb.Math.lerp(
-        rb.Game.current().camera.pos.x, camera_ideal, rb.Time.fixed_delta / 0.4
-    )
-
-
-# set the scene's update function
-level1.scene.fixed_update = fixed_update
 
 # begin the game
 rb.begin()
