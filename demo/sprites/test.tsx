628b2f27512244a8cbe6e21cabfaeacf4fa5741e
Martin Chaperot <martin.chapino@gmail.com>
2022-11-24 22:23:03 -0500

    tiled map

---
+++ demo/sprites/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="test" tilewidth="8" tileheight="8" tilecount="96" columns="8">
+ <image source="cavesofgallet_tiles.png" width="64" height="96"/>
+</tileset>
