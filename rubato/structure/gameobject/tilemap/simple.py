6936b8ca82fdafff25efacf3d71e165bded2fef9
Martin Chaperot <martin.chapino@gmail.com>
2022-11-30 19:09:06 -0500

    remove prop methods from simple

--- rubato/structure/gameobject/tilemap/simple.py
+++ rubato/structure/gameobject/tilemap/simple.py
@@ -53,82 +53,19 @@ class SimpleTilemap(Component):
         self.uptodate = False
         """Whether the tilemap is up to date."""
 
-    @property
-    def tilemap(self) -> list[list[int]]:
-        """
-        The tilemap array. If you change this, you must either set the array or set the uptodate property to False.
-        """
-        return self._map
-
-    @tilemap.setter
-    def tilemap(self, value: list[list[int]]):
-        self._map = value
-        self.uptodate = False
-
-    @property
-    def tiles(self) -> list[Surface]:
-        """
-        The tiles array. If you change this, you must either set the array or set the uptodate property to False.
-        """
-        return self._tiles
-
-    @tiles.setter
-    def tiles(self, value: list[Surface]):
-        self._tiles = value
-        self.uptodate = False
-
-    @property
-    def tile_size(self) -> Vector:
-        """
-        The size of each tile in the tilemap. If you change this, you must either set the array or set the uptodate
-        property to False.
-        """
-        return self._tile_size
-
-    @tile_size.setter
-    def tile_size(self, value: Vector | tuple[int, int]):
-        self._tile_size = Vector.create(value)
-        self.uptodate = False
-
-    @property
-    def collision(self) -> list[int]:
-        """
-        The list of integers representing the tiles that should have collision. If you change this, you must either set
-        the array or set the uptodate property to False.
-        """
-        return self._collision
-
-    @collision.setter
-    def collision(self, value: list[int]):
-        self._collision = value
-        self.uptodate = False
-
-    @property
-    def collider_tag(self) -> list[str]:
-        """
-        The list of strings representing the tags of the colliders. If you change this, you must either set the array or
-        set the uptodate property to False.
-        """
-        return self._collider_tag
-
-    @collider_tag.setter
-    def collider_tag(self, value: list[str]):
-        self._collider_tag = value
-        self.uptodate = False
-
     def _regen(self):
         dims = max([len(row) for row in self._map]), len(self._map)
-        self._result = Surface(int(dims[0] * self.tile_size.x), int(dims[1] * self.tile_size.y))
+        self._result = Surface(int(dims[0] * self._tile_size.x), int(dims[1] * self._tile_size.y))
 
         for i, row in enumerate(self._map):
-            y = (i * self.tile_size.y) - self._result.height / 2 + self.tile_size.y / 2
+            y = (i * self._tile_size.y) - self._result.height / 2 + self._tile_size.y / 2
             for j, tile in enumerate(row):
-                x = (j * self.tile_size.x) - self._result.width / 2 + self.tile_size.x / 2
+                x = (j * self._tile_size.x) - self._result.width / 2 + self._tile_size.x / 2
                 self._result.blit(self._tiles[tile], dst=(int(x), int(y)))
                 if tile in self._collision:
                     self.gameobj.add( # TODO: add to a child gameobject when that's a thing
                         Rectangle(
-                            *(self.tile_size * self.scale).tuple_int(),
+                            *(self._tile_size * self.scale).tuple_int(),
                             tag=self._collider_tag[tile] if tile < len(self._collider_tag) else "",
                             offset=(x, y) * self.scale,
                         )
