763db8997df8704ae96359b0e63730b923179bd3
Tomer Sedan <tomer.sedan@pm.me>
2022-12-04 14:40:53 -0500

    googify

--- demo/donut.py
+++ demo/donut.py
@@ -14,7 +14,7 @@ def draw():
     A += 0.0704
     B += 0.0352
 
-    z= [0.0] * di_sq
+    z = [0.0] * di_sq
     e, g = math.sin(A), math.cos(A)
     n, m = math.sin(B), math.cos(B)
     for j in range(0, 628, 7):
@@ -22,19 +22,18 @@ def draw():
         h = d + 1.5
         for i in range(0, 628, 2):
             c, l = math.sin(i / 100), math.cos(i / 100)
-            D = 1 / (c * h * e + f * g + 5)
+            D = 2 / (c * h * e + f * g + 5)
             t = c * h * g - f * e
-            x = int(hdi + 30 * D * (l * h * m - t * n))
-            y = int(hdi + 30 * D * (l * h * n + t * m))
+            x = int(hdi + 15 * D * (l * h * m - t * n))
+            y = int(hdi + 15 * D * (l * h * n + t * m))
             o = int(x + di * y)
-            N = int(8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n))
-            if di > y and y > 0 and x > 0 and di > x and D > z[o]:
+            if o < di_sq and D > z[o]:
                 z[o] = D
 
     s.fill(rb.Color.night)
     for k in range(di_sq):
         if z[k] != 0:
-            color = rb.Color.mix(rb.Color.yellow, rb.Color.red, z[k] * 2, "linear")
+            color = rb.Color.mix(rb.Color.yellow, rb.Color.red, z[k], "linear")
             s.set_pixel((k % di - hdi, k // di - hdi), color, False)
     rb.Draw.surface(s)
 
