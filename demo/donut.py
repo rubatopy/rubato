76a4174f68f5be3a5d44523981f5207c194bc485
Yamm Elnekave <52299477+mrElnekave@users.noreply.github.com>
2022-12-06 14:30:20 -0800

    New Donut (#430)
    * Update donut.py
    
    * googify
    
    * shape rotator
    
    * oui
    
    * w/ orientation bye bye kiss😘
    
    * actual
    
    * better renderer
    
    * Update 3d_renderer.py
    
    * control urself
    
    * donut redone
    
    * im done with this
    
    * ok
    
    * Update donut.py
    
    Co-authored-by: Tomer Sedan <tomer.sedan@pm.me>

--- demo/donut.py
+++ demo/donut.py
@@ -1,43 +1,52 @@
-import math, rubato as rb
+"""Prototype mathematics in rubato"""
+import rubato as rb
+import math
 
-A, B = 0, 0
-di, sc = 54, 3
-hdi, di_sq = di // 2, di * di
-res = rb.Vector(di * sc, di * sc)
+shape: list[tuple[float, float, float]] = []  # list of the points on the shape
+roll, pitch, yaw = 0, 0, 0  # x, z, and y rotation, respectively
+surf_dim = 50  # diameter of the surface holding the shape
 
-rb.init("donut demo", res, res * 3)
-s = rb.Surface(di, di, (sc, sc))
+rb.init("Donut Demo", res=(surf_dim, surf_dim), window_size=(500, 500), target_fps=60)
+surf = rb.Surface(surf_dim, surf_dim)
+
+# creates a donut and populates the shape list
+rad, thick = 12, 7  # radius and thickness of the donut
+mag = rad + thick  # maximum distance of any pt to the origin
+for i in range(0, 360, 4):  # revolve around a circle
+    for j in range(0, 360, 2):  # revolve the circle around the tube
+        v, u = math.radians(i), math.radians(j)
+        x = (rad + thick * math.cos(v)) * math.cos(u)
+        y = (rad + thick * math.cos(v)) * math.sin(u)
+        z = thick * math.sin(v)
+        shape.append((x, y, z))
+
+
+def rotate_pt(x, y, z, roll, pitch, yaw):
+    cosp, sinp = math.cos(pitch), math.sin(pitch)
+    cosy, siny = math.cos(yaw), math.sin(yaw)
+    cosr, sinr = math.cos(roll), math.sin(roll)
+
+    rx = (cosp * siny * sinr - sinp * cosr) * y + (sinp * sinr + cosp * siny * cosr) * z + cosp * cosy * x
+    ry = (cosp * cosr + sinp * siny * sinr) * y + (sinp * siny * cosr - cosp * sinr) * z + sinp * cosy * x
+    rz = (cosy * sinr) * y + (cosy * cosr) * z - siny * x
+
+    return int(rx), int(ry), int(rz)
 
 
 def draw():
-    global A, B
-    A += 0.0704
-    B += 0.0352
+    global roll, pitch, yaw
+    roll += 0.0704
+    pitch += 0.0352
 
-    z, b = [0.0] * di_sq, [-1] * di_sq
-    e, g = math.sin(A), math.cos(A)
-    n, m = math.sin(B), math.cos(B)
-    for j in range(0, 628, 7):
-        f, d = math.sin(j / 100), math.cos(j / 100)
-        h = d + 1.5
-        for i in range(0, 628, 2):
-            c, l = math.sin(i / 100), math.cos(i / 100)
-            D = 1 / (c * h * e + f * g + 5)
-            t = c * h * g - f * e
-            x = int(hdi + 30 * D * (l * h * m - t * n))
-            y = int(hdi + 30 * D * (l * h * n + t * m))
-            o = int(x + di * y)
-            N = int(8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n))
-            if di > y and y > 0 and x > 0 and di > x and D > z[o]:
-                z[o] = D
-                b[o] = (N + 1) if N > 0 else 0
-
-    s.fill(rb.Color.night)
-    for k in range(di_sq):
-        if b[k] != -1:
-            color = rb.Color.mix(rb.Color.yellow, rb.Color.red, b[k] / 12, "linear")
-            s.set_pixel((k % di - hdi, k // di - hdi), color, False)
-    rb.Draw.surface(s)
+    z_buffer = [-float("inf")] * (surf_dim**2)
+    surf.fill(rb.Color.night)
+    for point in shape:
+        x, y, z = rotate_pt(*point, roll, pitch, yaw)
+        if z_buffer[x + surf_dim * y] < z:
+            z_buffer[x + surf_dim * y] = z
+            color = rb.Color.mix(rb.Color.yellow, rb.Color.red, rb.Math.map(z, -mag, mag, 0, 1), "linear")
+            surf.set_pixel((x, y), color, False)
+    rb.Draw.surface(surf)
 
 
 rb.Game.draw = draw
