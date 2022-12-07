b89c451be2e1ec700c3213a9dfc419fd9d552656
Martin Chaperot <martin.chapino@gmail.com>
2022-12-06 21:53:36 -0500

    fix pyinstaller (#433)
    * fix pyinstaller
    
    * update settings.json
    
    * yo mama
    
    Co-authored-by: Yamm Elnekave <klavlavigugu@gmail.com>

--- rubato/__pyinstaller/hook-rubato.py
+++ rubato/__pyinstaller/hook-rubato.py
@@ -1,5 +1,16 @@
 """The PyInstaller hooks file for rubato."""  # pylint: disable=invalid-name
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
 datas = collect_data_files("rubato")
 datas += collect_data_files("sdl2dll")
+
+hiddenimports = [
+    "sdl2",
+    "sdl2.sdlttf",
+    "sdl2.ext",
+    "sdl2.sdlmixer",
+    "sdl2dll",
+    "Cython",
+    "pytiled_parser",
+]
+hiddenimports += collect_submodules("rubato")
